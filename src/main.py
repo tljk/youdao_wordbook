6d82261bf41990ac8b0065a8bade06dff1b2021d
tanglinchuanz@163.com <tanglinchuanz@163.com>
2022-08-10 17:39:39 +0800

    improve 切换速度大幅提升，add epaper休眠

--- src/main.py
+++ src/main.py
@@ -10,8 +10,6 @@ import uasyncio
 import random
 import wbconfig
 import machine
-import phonetic
-import framebuf
 import gc
 import time
 import esp32
@@ -32,7 +30,6 @@ class Wordbook:
             self.count = random.randint(100,5000) # the count number traverse to next word
         self.epd.Clear()
         self.epd.fill(0xff)
-        self.word = ''
         
         self.crawlerTSF = uasyncio.ThreadSafeFlag()
         self.epdTSF = uasyncio.ThreadSafeFlag()
@@ -72,7 +69,6 @@ class Wordbook:
                         if self.wifi.isconnected():
                             break
                 
-                await uasyncio.sleep(3) 
                 if not self.wifi.isconnected():
                     print('wifi connect failed')
                 else:
@@ -120,11 +116,14 @@ class Wordbook:
                     self.pause -= 1
                 else:
                     print('sleep')
+                    self.epd.sleep()
                     await uasyncio.sleep(0.5)
                     machine.lightsleep(1000*60*60)
                     # print(machine.wake_reason())
                     if machine.wake_reason() == 4:
                         self.crawlerTSF.set()
+                    else:
+                        self.epd.reset()
 
     async def screen(self):
         print('screen init')
@@ -139,85 +138,19 @@ class Wordbook:
                     if self.count > 0:
                         self.count -= 1
                     else:
-                        # print('load data')
-                        gc.collect()
-                        json = ujson.loads(w[1].decode('utf-8'))
-
-                        # word and index
-                        self.epd.fill(0xff)
-                        self.epd.font_set(0x23,0,1,0)
-                        self.epd.text(w[0], 0, 0, 0x00)                                                
-                        self.epd.font_set(0x21,0,1,0)
-                        self.epd.text(str(index),250-len(str(index))*6,110,0x00)
-
-                        # phonetic
-                        offset = 4
-                        for c in json['p']:
-                            if phonetic.font.get(c):
-                                font_buf = bytearray(phonetic.font.get(c))
-                                font_framebuffer = framebuf.FrameBuffer(font_buf, len(font_buf)//3, 24, framebuf.MONO_HLSB)
-                                self.epd.blit(font_framebuffer, len(w[0])*12+offset, 0)
-                                if c in '(),-.:;[]ˈˌː':
-                                    offset+=4
-                                elif c in 'mwæ':
-                                    offset+=12
-                                else:
-                                    offset+=len(font_buf)//3
-
-                        # trans font selection
-                        count = len(json['t'].encode())
-                        length = len(json['t'])
-                        y = (count - length)//2
-                        x = length - y
-                        count = x + 2 * y
-
-                        # font24 10 * 4 = 40
-                        # font16 15 * 6 = 90
-                        # font12 20 * 8 = 160
-                        composeSet = {}
-                        if count < (10) * 4 * 2:
-                            self.epd.font_set(0x23,0,1,0)
-                            composeSet = {'size': 24, 'width': 10, 'height': 4}
-                        elif count < (15) * 6 * 2:
-                            self.epd.font_set(0x22,0,1,0)
-                            composeSet = {'size': 16, 'width': 15, 'height': 6}
-                        elif count < (20) * 8 * 2:
-                            self.epd.font_set(0x21,0,1,0)
-                            composeSet = {'size': 12, 'width': 20, 'height': 8}
-                        else:
-                            self.epd.font_set(0x21,0,1,0)
-                            composeSet = {'size': 12, 'width': 20, 'height': 8}
-
-                        # trans each line
-                        curr = 0
-                        for i in range(composeSet['height']):
-                            line = ''
-                            count = composeSet['width']*2
-                            while curr < len(json['t']):
-                                if len(json['t'][curr].encode()) == 3:
-                                    count -= 2
-                                else:
-                                    count -= 1
-                                if count < 0:
-                                    break
-                                line += json['t'][curr]
-                                curr += 1
-                            self.epd.text(line, 0, 25 + composeSet['size'] * i, 0x00)
-                            if curr >= len(json['t']):
-                                break
-                            # print(line)
-
-                        self.epd.display_Partial(self.epd.buffer)
-                        self.pause = 10
-                        machine.freq(80000000)
-
                         await self.epdTSF.wait() # critical point
 
                         machine.freq(240000000)
-                        self.epd.TurnOnDisplayPart()
+                        # print('load data')
+                        gc.collect()
+                        self.epd.frame(index, w[0], ujson.loads(w[1].decode('utf-8')))  
+                        self.epd.display_Partial(self.epd.bufferTrans)
+
+                        self.pause = 30
                         self.nvs.set_i32('curr',index)
                         self.nvs.commit()
                         self.count = 0
+                        machine.freq(80000000)
                         
             except KeyboardInterrupt:
                 break
